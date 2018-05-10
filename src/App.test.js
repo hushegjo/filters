79c7f1093608a9e7cc8dfe49e302ac109bc4acb7
József Hushegyi <jhu@mediagate.hu>
2018-05-10 15:13:16 +0200

    Set up test env

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders withot chrashing', () => {
+  const wrapper = shallow(<App />);
+  console.log(wrapper.debug());
 });
